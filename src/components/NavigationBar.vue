8063d1bfb520bc998e7a5c814c76f56ffdf41213
Falk Felix <felix.falk2903@gmail.com>
2022-03-21 16:52:51 +0100

    merging with master

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app-bar color="green darken-3">
-    <v-tabs>
+  <v-app-bar color="green darken-3" class="">
+    <v-tabs class="">
       <v-tab value="Home" to="/"> home </v-tab>
       <v-tab value="About" to="/about">About </v-tab>
     </v-tabs>
