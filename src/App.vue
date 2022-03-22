1acd235351115bfbb1288377d26fbfe24780684a
Falk Felix <felix.falk2903@gmail.com>
2022-03-22 15:15:36 +0100

    implemented a basic footer, still desighning it #9, #3

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <v-app>
     <navigation-bar />
     <router-view />
+    <footerBar/>
   </v-app>
 </template>
 
 <script>
 import NavigationBar from "@/components/NavigationBar.vue";
+import footerBar from '@/components/Footer.vue';
 export default {
   components: {
     NavigationBar,
+    footerBar
   },
 };
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-footer color="primary"> 
+      <v-row justify="center">
+          <v-col cols="2">
+              <p>boe</p>
+              <p>Line2</p>
+
+          </v-col>
+
+          <v-col cols="2">
+              <p>boe</p>
+          </v-col>
+
+          <v-col cols="2">
+              <p>boe</p>
+          </v-col>
+
+          <v-col cols="2">
+              <p>boe</p>
+          </v-col>
+      </v-row>
+
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "footerBar",
+};
+</script>
+
+<style>
+</style>
