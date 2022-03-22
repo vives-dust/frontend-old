5adad3304d1e9469d1bddf54dd1e7687435b64e7
Falk Felix <felix.falk2903@gmail.com>
2022-03-22 15:46:38 +0100

    stoped for a second with footernd with footer,and working on issue #7 now

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,25 +1,24 @@
 <template>
-  <v-footer color="primary"> 
-      <v-row justify="center">
-          <v-col cols="2">
-              <p>boe</p>
-              <p>Line2</p>
+  <v-footer class="text-medium-emphasis" color="secondary">
+    <v-row justify="center">
+      <v-col cols="3">
+        <h2 >Dust</h2>
+        <p class="mt-3">Making the world drink enough water</p>
 
-          </v-col>
+      </v-col>
 
-          <v-col cols="2">
-              <p>boe</p>
-          </v-col>
+      <v-col cols="2">
+        <p>boe</p>
+      </v-col>
 
-          <v-col cols="2">
-              <p>boe</p>
-          </v-col>
+      <v-col cols="2">
+        <p>boe</p>
+      </v-col>
 
-          <v-col cols="2">
-              <p>boe</p>
-          </v-col>
-      </v-row>
-
+      <v-col cols="2">
+        <p>boe</p>
+      </v-col>
+    </v-row>
   </v-footer>
 </template>
 
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,10 +1,38 @@
 // Styles
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
-
-// Vuetify
-import { createVuetify } from 'vuetify'
-
-export default createVuetify(
+import { aliases, fa } from 'vuetify/lib/iconsets/fa'
+import { mdi } from 'vuetify/lib/iconsets/mdi'
+//Vuetify
+import { createVuetify, ThemeDefinition } from 'vuetify'
+// const myCustomLightTheme: ThemeDefinition = {   
+//     dark: false,
+//     colors: {
+//       background: '#FFFFFF',
+//       surface: '#FFFFFF',
+//       primary: '#6200EE',
+//       'primary-darken-1': '#3700B3',
+//       secondary: '#03DAC6',
+//       'secondary-darken-1': '#018786',
+//       error: '#B00020',
+//       info: '#2196F3',
+//       success: '#4CAF50',
+//       warning: '#FB8C00',
+//     }
+//   }
   
+
+
+
+
+export default createVuetify({
+    icons: {
+      defaultSet: 'fa',
+      aliases,
+      sets: {
+        fa,
+        mdi,
+      }
+    },
+  }
 )
