f77798fb52284b0ead3e351a60e69bededd3b996
Falk Felix <felix.falk2903@gmail.com>
2022-03-23 10:02:48 +0100

    working on #2 #3 and #19

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,9 +1,11 @@
 <template>
   <v-card color="primary" height="50">
-    <v-tabs centered>
-      <v-tab value="Home" to="/"> Home </v-tab>
-      <v-tab value="About" to="/about">About </v-tab>
-    </v-tabs>
+    <v-row justify="center" align="center">
+      <v-tabs centered>
+        <v-tab value="Home" to="/"> Home </v-tab>
+        <v-tab value="About" to="/about">About </v-tab>
+      </v-tabs>
+    </v-row>
   </v-card>
 </template>
 
