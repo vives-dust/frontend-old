11afcc1b3c67b8169251a258c6a9cdee6b2193d4
Falk Felix <felix.falk2903@gmail.com>
2022-03-23 09:56:41 +0100

    forgot to switch to branch Basic-layout but worked on #9 and #2

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,25 +2,50 @@
   <v-footer class="text-medium-emphasis mt-5" color="fourth">
     <v-row justify="center">
       <v-col cols="3">
-        <h2>Dust</h2>
-        <p class="mt-3">Making the world drink enough water</p>
-        <v-col class="m">
-          <v-icon class="mx-5" size="50" icon="mdi:mdi-car-cruise-control " />
-          <v-icon class= "mx-5" size="50" icon="mdi:mdi-id-card" />
-          <v-icon class="mx-5" size="50" icon="mdi:mdi-jquery " />
+        <h1>DUST</h1>
+        <p class="mt-3 ">Making the world drink enough water</p>
+        <p >Just another paragraph for the sake of it</p>
+        <v-col class="mt-3">
+          <v-icon class="mr-8 ml-n4" size="60" icon="mdi:mdi-car-cruise-control " />
+          <v-icon class= "mx-8" size="60" icon="mdi:mdi-id-card" />
+          <v-icon class="mx-8" size="60" icon="mdi:mdi-github" />
         </v-col>
       </v-col>
 
-      <v-col cols="2">
-        <p>boe</p>
+      <v-col cols="2" >
+        <h3 class="text-disabled mb-5 mt-2">LINKS</h3>
+
+                <p class="my-2">link one</p>
+        <p class="my-2">link two</p>
+        <p class="my-2">link three</p>
+        <p class="my-2">link four</p>
       </v-col>
 
       <v-col cols="2">
-        <p>boe</p>
+        <h3 class="text-disabled mb-5 mt-2">SUPPORT</h3>
+
+                <p class="my-2">link one</p>
+        <p class="my-2">link two</p>
+        <p class="my-2">link three</p>
+        <p class="my-2">link four</p>
       </v-col>
 
       <v-col cols="2">
-        <p>boe</p>
+        <h3 class="text-disabled mb-5 mt-2">LEGAL</h3>
+
+                <p class="my-2">link one</p>
+        <p class="my-2">link two</p>
+        <p class="my-2">link three</p>
+        <p class="my-2">link four</p>
+      </v-col>
+
+      <v-col cols="auto">
+        <h3 class="text-disabled mb-5 mt-2">SITEMAP</h3>
+
+        <p class="my-2">link one</p>
+        <p class="my-2">link two</p>
+        <p class="my-2">link three</p>
+        <p class="my-2">link four</p>
       </v-col>
     </v-row>
   </v-footer>
