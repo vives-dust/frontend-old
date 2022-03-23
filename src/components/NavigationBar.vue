0e4864106617bad6fc78c7f3b6ecf5a9c4abd57d
Falk Felix <felix.falk2903@gmail.com>
2022-03-23 16:29:03 +0100

    was working on #24 and discovered #25

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,16 +1,28 @@
 <template>
-  <v-card color="fifth" height="auto">
-    <v-row no-gutters >
-      <v-tabs centered class="mt-2">
-        <v-tab value="Home" to="/"> Home </v-tab>
-        <v-tab value="About" to="/about">About </v-tab>
-      </v-tabs>
-      <v-spacer />
-      <v-icon
-        class="mt-3 mx-5"
-        size="30"
-        icon="mdi:mdi-web"
-      />
+  <v-card color="fifth" height="">
+    <v-row>
+      <v-col cols="1"> </v-col>
+      <v-col cols="10" class="text-center">
+        <v-tabs centered class="mt-2">
+          <v-tab value="Home" to="/"> Home </v-tab>
+          <v-tab value="About" to="/about">About </v-tab>
+        </v-tabs>
+      </v-col>
+
+      <v-col cols="1" class="text-right">
+        <v-menu transition="fade-transition">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn v-bind="attrs" v-on="on">
+              <v-icon class="mt-3 mx-5" size="33" icon="mdi:mdi-web" />
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="n in 5" :key="n">
+              <v-list-item-title v-text="'Item ' + n"></v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </v-col>
     </v-row>
   </v-card>
 </template>
