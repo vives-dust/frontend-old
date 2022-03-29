538e09023af87357b4f2f807cc02cb1cef41c328
Falk Felix <felix.falk2903@gmail.com>
2022-03-29 09:43:49 +0200

    trying to implement selection tool

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,28 +1,35 @@
 <template>
-  <v-card color="fifth" height="">
-    <v-row>
-      <!-- <v-col cols="1"> </v-col> -->
-      <v-col cols="12" class="text-center">
-        <v-tabs centered class="mt-2">
+  <v-card color="fifth" height="auto">
+    <v-row no-gutters>
+      <v-col cols="1"> </v-col>
+      <v-col cols="10" class="text-center">
+        <v-tabs centered class="mt-2" height="auto">
           <v-tab value="Home" to="/"> Home </v-tab>
           <v-tab value="About" to="/about">About </v-tab>
         </v-tabs>
       </v-col>
 
-      <!-- <v-col cols="1" class="text-right">
+      <v-col cols="1" class="text-right">
         <v-menu transition="fade-transition">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn v-bind="attrs" v-on="on">
-              <v-icon class="mt-3 mx-5" size="33" icon="mdi:mdi-web" />
-            </v-btn>
-          </template>
+          <v-btn
+            class="mt-3 mx-5"
+            color="fifth"
+            elevation="0"
+            @click="toggleshowingLanguageSelectionTool"
+          >
+            <v-icon size="33" icon="mdi:mdi-web" />
+          </v-btn>
           <v-list>
-            <v-list-item v-for="n in 5" :key="n">
+            <v-list-item
+              v-for="n in 5"
+              :key="n"
+              v-model="showingLanguageSelectionTool"
+            >
               <v-list-item-title v-text="'Item ' + n"></v-list-item-title>
             </v-list-item>
           </v-list>
         </v-menu>
-      </v-col> -->
+      </v-col>
     </v-row>
   </v-card>
 </template>
@@ -30,6 +37,14 @@
 <script>
 export default {
   name: "NavigationBar",
+  data() {
+    showingLanguageSelectionTool: false;
+  },
+  methods:{
+    toggleshowingLanguageSelectionTool(){
+      this.showingLanguageSelectionTool = !this.showingLanguageSelectionTool
+    }
+  }
 };
 </script>
 
