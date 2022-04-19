b220c73f6b97ef85a4764b34f7abe19879482e70
Falk Felix <felix.falk2903@gmail.com>
2022-04-19 08:55:21 +0200

    added pins to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,28 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    pins: [
+      {
+        id: 1,
+        x: 51.037861,
+        y: 4.240528,
+      },
+      {
+        id: 2,
+        x: 51.047861,
+        y: 4.240528,
+      },
+      {
+        id: 3,
+        x: 51.147861,
+        y: 4.240528,
+      },
+      {
+        id: 4,
+        x: 51.247861,
+        y: 4.240528,
+      },
+    ],
   },
   getters: {
   },
