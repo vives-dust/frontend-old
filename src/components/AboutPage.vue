d948d7738f094a00fcaaef8cc63f5ba411c6658b
Falk Felix <felix.falk2903@gmail.com>
2022-04-20 09:31:02 +0200

    implemented #32 but turning the opacity down din't look right, so i elevated it on hover

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -18,40 +18,48 @@
   <v-row justify="center">
     <v-col cols="1"> </v-col>
     <v-col cols="2" md="2">
-      <v-card height="500">
-        <v-img height="130" src="../assets/sensor.jpg"> </v-img>
-        <h2 class="ma-3">{{ $t("aboutPage.card1.title") }}</h2>
-        <p class="ml-3">
-          {{ $t("aboutPage.card1.text") }}
-        </p>
-      </v-card>
+      <v-hover v-slot="{ isHovering, props }">
+        <v-card height="500" v-bind="props" :elevation="isHovering ? 16 : 2">
+          <v-img height="130" src="../assets/sensor.jpg"> </v-img>
+          <h2 class="ma-3">{{ $t("aboutPage.card1.title") }}</h2>
+          <p class="ml-3">
+            {{ $t("aboutPage.card1.text") }}
+          </p>
+        </v-card>
+      </v-hover>
     </v-col>
     <v-col cols="2" md="2">
-      <v-card height="500">
-        <v-img height="130" src="../assets/bier2.jpg"> </v-img>
-        <h2 class="ma-3">{{ $t("aboutPage.card2.title") }}</h2>
-        <p class="ml-3">
-          {{ $t("aboutPage.card2.text") }}
-        </p>
-      </v-card>
+      <v-hover v-slot="{ isHovering, props }">
+        <v-card height="500" v-bind="props" :elevation="isHovering ? 16 : 2">
+          <v-img height="130" src="../assets/bier2.jpg"> </v-img>
+          <h2 class="ma-3">{{ $t("aboutPage.card2.title") }}</h2>
+          <p class="ml-3">
+            {{ $t("aboutPage.card2.text") }}
+          </p>
+        </v-card>
+      </v-hover>
     </v-col>
     <v-col cols="2" md="2">
-      <v-card height="500">
-        <v-img height="130" src="../assets/vlaanderen1.jpg"> </v-img>
-        <h2 class="ma-3">{{ $t("aboutPage.card3.title") }}</h2>
-        <p class="ml-3">
-          {{ $t("aboutPage.card3.text") }}
-        </p>
-      </v-card>
+      <v-hover v-slot="{ isHovering, props }">
+        <v-card height="500" v-bind="props" :elevation="isHovering ? 16 : 2">
+          <v-img height="130" src="../assets/vlaanderen1.jpg"> </v-img>
+          <h2 class="ma-3">{{ $t("aboutPage.card3.title") }}</h2>
+          <p class="ml-3">
+            {{ $t("aboutPage.card3.text") }}
+          </p>
+        </v-card>
+      </v-hover>
     </v-col>
     <v-col cols="2" md="2">
-      <v-card height="500">
-        <v-img height="130" src="../assets/VIVES_Logo.png"> </v-img>
-        <h2 class="ma-3">{{ $t("aboutPage.card4.title") }}</h2>
-        <p class="ml-3">
-          {{ $t("aboutPage.card4.text") }}
-        </p>
-      </v-card>
+      <v-hover v-slot="{ isHovering, props }">
+        <v-card height="500" v-bind="props" :elevation="isHovering ? 16 : 2">
+          <v-img height="130" src="../assets/VIVES_Logo.png"> </v-img>
+          <h2 class="ma-3">{{ $t("aboutPage.card4.title") }}</h2>
+          <p class="ml-3">
+            {{ $t("aboutPage.card4.text") }}
+          </p>
+        </v-card>
+      </v-hover>
     </v-col>
     <v-col cols="1"> </v-col>
   </v-row>
@@ -62,6 +70,5 @@ export default {
   name: "aboutPage",
 };
 </script>
-
 <style>
 </style>
