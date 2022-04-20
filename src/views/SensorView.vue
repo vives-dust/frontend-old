0a1e2d8238c8bfead6122d63ca86263db7757da7
Falk Felix <felix.falk2903@gmail.com>
2022-04-20 14:53:40 +0200

    added 2 more test line charts to get a basic layout see #30

--- src/views/SensorView.vue
+++ src/views/SensorView.vue
@@ -16,6 +16,14 @@
       <v-card class="my-5">
         <lineChart :data="lineChart1Data" />
       </v-card>
+
+      <v-card class="my-5">
+        <lineChart :data="lineChart2Data" />
+      </v-card>
+
+      <v-card class="my-5">
+        <lineChart :data="lineChart3Data" />
+      </v-card>
     </v-col>
   </v-row>
 </template>
@@ -79,24 +87,73 @@ export default {
             data: [40, 39, 10, 40, 39, 80, 100],
             borderColor: "#E6AC1C",
             backgroundColor: "#E6AC1C",
-            fill: true,
+            fill: false,
             tension: 0.4,
+            pointHitRadius: 100,
           },
           {
             label: "soil moisture level 2",
-            data: [80, 90, 70, 100,70, 110, 60],
+            data: [80, 90, 70, 100, 70, 110, 60],
             borderColor: "#49E658",
             backgroundColor: "#49E658",
-            fill: true,
+            fill: false,
             tension: 0.4,
+            pointHitRadius: 100,
           },
           {
             label: "soil moisture level 3",
             data: [150, 140, 130, 160, 150, 120, 150],
             borderColor: "#E6B332",
             backgroundColor: "#E6B332",
+            fill: false,
+            tension: 0.4,
+            pointHitRadius: 100,
+          },
+        ],
+      },
+
+      lineChart2Data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        datasets: [
+          {
+            label: "Temperature",
+            data: [40, 39, 10, 40, 39, 80, 100],
+            borderColor: "#B34E3E",
+            backgroundColor: "#B34E3E ",
             fill: true,
             tension: 0.4,
+            pointHitRadius: 100,
+          },
+        ],
+      },
+
+      lineChart3Data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+        ],
+        datasets: [
+          {
+            label: "Light",
+            data: [50, 100, 100, 20, 150, 10, 80],
+            borderColor: "#E0E600",
+            backgroundColor: "#E0E600 ",
+            fill: true,
+            tension: 0.4,
+            pointHitRadius: 100,
           },
         ],
       },
