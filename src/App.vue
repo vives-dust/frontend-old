06268a8e7e93ac2c12ba70bb4acf1ab170de76a1
Falk Felix <felix.falk2903@gmail.com>
2022-04-22 17:10:08 +0200

    solved bug where there was anoying white space to the right on mobile, even with scrollbar disabled

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,13 @@ export default {
 @media screen and (max-width: 700px) {
   html {
     scrollbar-width: none;
+    overflow-x: clip;
+  }
+  html,body{
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
   }
 }
 </style>
