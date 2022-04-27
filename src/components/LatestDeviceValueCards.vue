692e8b45e8b1ae414838b43ca331b1f0a25189c7
Falk Felix <felix.falk2903@gmail.com>
2022-04-27 10:07:35 +0200

    Implemented the cards #48 for mobile

--- src/components/LatestDeviceValueCards.vue
+++ src/components/LatestDeviceValueCards.vue
@@ -1,11 +1,16 @@
 <template>
   <v-row justify="center" class="text-center mb-2">
-    <v-col v-for="sensor in device.sensors" :key="sensor._id">
+    <v-col
+      v-for="sensor in device.sensors"
+      :key="sensor._id"
+      cols="4"
+      class="hidden-xs"
+    >
       <v-card color="third">
         <v-card-title> {{ sensor.field }} </v-card-title>
         <v-card-text class="d-inline-flex align-center">
           <v-icon
-            class="mr-8 ml-n4"
+            class="mr-8"
             size="60"
             icon="mdi:mdi-thermometer "
             v-if="sensor.type == 'temperature'"
@@ -13,13 +18,13 @@
           <p v-if="sensor.type == 'temperature'">{{ sensor.value }} &deg;C</p>
 
           <v-icon
-            class="mr-8 ml-n4"
+            class="mr-8"
             size="60"
             icon="mdi:mdi-water-percent"
             v-if="sensor.type == 'moisture'"
           />
           <v-icon
-            class="mr-8 ml-n4"
+            class="mr-8"
             size="60"
             icon="mdi:mdi-tailwind"
             v-if="sensor.type == 'pressure'"
@@ -29,32 +34,39 @@
         </v-card-text>
       </v-card>
     </v-col>
+
+    <v-col
+      class="hidden-sm-and-up"
+      v-for="sensor in device.sensors"
+      :key="sensor._id"
+      cols="6"
+    >
+      <v-card color="third" class="mx-n2 my-n2">
+        <v-card-title class="ml-n3 mb-n6 mt-n2"> {{ sensor.field }} </v-card-title>
+        <v-card-text class="d-inline-flex align-center">
+          <v-icon
+            size="45"
+            icon="mdi:mdi-thermometer "
+            v-if="sensor.type == 'temperature'"
+          />
+          <p v-if="sensor.type == 'temperature'">{{ sensor.value }} &deg;C</p>
+
+          <v-icon
+            size="45"
+            icon="mdi:mdi-water-percent"
+            v-if="sensor.type == 'moisture'"
+          />
+          <v-icon
+            size="45"
+            icon="mdi:mdi-tailwind"
+            v-if="sensor.type == 'pressure'"
+          />
+
+          <p v-if="sensor.type != 'temperature'">{{ sensor.value }}</p>
+        </v-card-text>
+      </v-card>
+    </v-col>
   </v-row>
-
-  <!-- <v-card class="mx-2" color="third">
-    <v-card-title> temperature </v-card-title>
-    <v-card-text class="d-inline-flex align-center">
-      <v-icon class="mr-8 ml-n4" size="60" icon="mdi:mdi-thermometer " />
-      <p>temp &deg;C</p>
-    </v-card-text>
-  </v-card>
-
-  <v-card class="mx-2" color="third">
-    <v-card-title> humidity </v-card-title>
-    <v-card-text class="d-inline-flex align-center">
-      <v-icon class="mr-8 ml-n4" size="60" icon="mdi:mdi-water-percent" />
-
-      <p>something</p>
-    </v-card-text>
-  </v-card> -->
-
-  <!-- <v-card class="mx-2" color="third">
-    <v-card-title> pressure </v-card-title>
-    <v-card-text class="d-inline-flex align-center">
-      <v-icon class="mr-8 ml-n4" size="60" icon="mdi:mdi-tailwind" />
-      <p>{{ device }}</p>
-    </v-card-text>
-  </v-card> -->
 </template>
 
 <script>
