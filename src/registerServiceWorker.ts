dd54f1d143fae16f50f828b9993f7b7d4554fb6a
Falk Felix <felix.falk2903@gmail.com>
2022-05-16 14:06:55 +0200

    Testing service workers

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -3,8 +3,7 @@
 import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
-  console.log(process.env.NODE_ENV)
-  register(`${process.env.BASE_URL}service-worker.js`, {
+  register(`${process.env.BASE_URL}/service-worker.js`, {
     ready () {
       console.log(
         'App is being served from cache by a service worker.\n' +
