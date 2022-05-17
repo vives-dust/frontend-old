6b76eb4e06f56191bad6bf082de0c54b6380c4a9
Falk Felix <felix.falk2903@gmail.com>
2022-05-17 15:08:23 +0200

    Last test for pwa reverting chages.

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,4 +1,23 @@
 <template>
+  <v-row justify="center" class="mt-8">
+    <v-col cols="11" sm="8">
+
+      <div class="text-center mt-5">
+        <v-card elevation="2" shaped :height="xs ? 150 : 300" width="auto">
+          <v-img
+            src="../assets/vlaanderen1.jpg"
+            :height="xs ? 150 : 300"
+            width="auto"
+          ></v-img>
+          <!-- All picture are under the creative common license -->
+        </v-card>
+
+        <p class="text-h5 text-justify mt-5 mb-16">
+          {{ $t("aboutPage.introduction") }}
+        </p>
+      </div>
+    </v-col>
+  </v-row>
 
   <v-row justify="center" class="hidden-xs">
     <v-col cols="1"> </v-col>
@@ -49,6 +68,62 @@
     <v-col cols="1"> </v-col>
   </v-row>
 
+  <!-- FOR MOBILE -->
+  <v-row justify="center" class="hidden-sm-and-up">
+    <v-col cols="11" class="hidden-sm-and-up">
+      <v-divider class="mt-n15 mb-6"></v-divider>
+      <v-card height="400">
+        <v-card max-height="150" elevation="0">
+          <v-img height="strech" src="../assets/sensor.jpg"> </v-img>
+        </v-card>
+        <h2 class="ma-3">{{ $t("aboutPage.card1.title") }}</h2>
+        <p class="ml-3">
+          {{ $t("aboutPage.card1.text") }}
+        </p>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row justify="center" class="hidden-sm-and-up">
+    <v-col cols="11">
+      <v-card height="400">
+        <v-card height="150" elevation="0">
+          <v-img height="strech" src="../assets/bier2.jpg"> </v-img>
+        </v-card>
+        <h2 class="ma-3">{{ $t("aboutPage.card2.title") }}</h2>
+        <p class="ml-3">
+          {{ $t("aboutPage.card2.text") }}
+        </p>
+      </v-card>
+    </v-col>
+  </v-row>
+
+  <v-row justify="center" class="hidden-sm-and-up">
+    <v-col cols="11">
+      <v-card height="400">
+        <v-card height="150" elevation="0">
+          <v-img height="strech" src="../assets/vlaanderen1.jpg"> </v-img>
+        </v-card>
+        <h2 class="ma-3">{{ $t("aboutPage.card3.title") }}</h2>
+        <p class="ml-3">
+          {{ $t("aboutPage.card3.text") }}
+        </p>
+      </v-card>
+    </v-col>
+  </v-row>
+
+  <v-row justify="center" class="hidden-sm-and-up">
+    <v-col cols="11">
+      <v-card height="400">
+        <v-card height="150" elevation="0">
+          <v-img height="strech " src="../assets/VIVES_Logo.png"> </v-img>
+        </v-card>
+        <h2 class="ma-3">{{ $t("aboutPage.card4.title") }}</h2>
+        <p class="ml-3">
+          {{ $t("aboutPage.card4.text") }}
+        </p>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script lang="ts">
