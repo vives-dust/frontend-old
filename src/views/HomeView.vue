9136319bb758b0066d33dce74e5e9855a6f8bfc8
Falk Felix <felix.falk2903@gmail.com>
2022-05-24 14:11:25 +0200

    Implemented mobile integration for updated layout in home page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,11 @@
 <template>
   <v-row justify="center">
-    <v-col cols="12">
-      <v-card elevation="5">
+    <v-col cols="11" sm="12">
+      <v-card elevation="5" class="hidden-sm-and-up">
+        <v-img src="@/assets/leaves.jpg" height="auto" />
+      </v-card>
+
+      <v-card elevation="5" class="hidden-xs">
         <v-img src="@/assets/leaves300-300.jpg" class="mb-5" width="100%" />
       </v-card>
     </v-col>
@@ -9,7 +13,9 @@
   <v-row justify="center">
     <v-col cols="11" sm="10">
       <!-- <carouselVue class="mt-16" /> -->
-      <mapVue class="mt-5 mb-10" />
+      <mapVue class="mt-5 mb-10 hidden-xs" />
+      <mapVue class="mt-2 mb-2 hidden-sm-and-up" />
+
     </v-col>
   </v-row>
   <v-row
