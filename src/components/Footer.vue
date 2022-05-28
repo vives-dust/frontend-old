34506950741aaa0d6d0c0cbf9905afa119ebf926
Falk Felix <felix.falk2903@gmail.com>
2022-05-28 16:42:35 +0200

    Changed picture and layout on home page.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer class="text-medium-emphasis" color="fourth">
+  <v-footer class="text-medium-emphasis mt-16" color="fourth">
     <v-row justify="center" class="hidden-xs">
       <v-col cols="3">
         <h1>DUST</h1>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,28 +1,38 @@
 <template>
-  <v-row justify="center">
+  <v-row>
     <v-col cols="11" sm="12">
       <v-card elevation="5" class="hidden-sm-and-up">
-        <v-img src="@/assets/leaves.jpg" height="auto" />
+        <v-img src="@/assets/nature.jpg" height="150" cover />
       </v-card>
 
-      <v-card elevation="5" class="hidden-xs">
-        <v-img src="@/assets/leaves300-300.jpg" class="mb-5" width="100%" />
+      <v-card elevation="5" class="hidden-xs mt-10">
+        <v-img src="@/assets/nature.jpg" class="mb-5" height="400" cover>
+          <v-row class="text-center" justify="center">
+            <v-col cols="12">
+              <v-container class="mt-16">
+                <h1 class="text-h2 font-weight-bold mb-4 mt-12" style="color:white">DUST</h1>
+
+                <h4 class="text-h4 font-weight-thin" style="color:white">Do nature a favor</h4>
+              </v-container>
+            </v-col>
+          </v-row>
+        </v-img>
       </v-card>
     </v-col>
   </v-row>
   <v-row justify="center">
     <v-col cols="11" sm="10">
-      <mapVue class="mt-5 mb-10 hidden-xs" />
+      <mapVue class="my-15 hidden-xs" />
       <mapVue class="mt-2 mb-2 hidden-sm-and-up" />
     </v-col>
   </v-row>
   <v-row
-    no-gutters
     justify="center"
-    style="background: rgb(var(--v-theme-backgroundShift), 0.5)"
+    style="background: rgb(var(--v-theme-backgroundShift), 0.2)"
+    class="py-15"
   >
     <v-col cols="11" sm="10">
-      <informationCards class="mb-10" />
+      <informationCards />
     </v-col>
   </v-row>
 </template>
